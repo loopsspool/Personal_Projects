54a7112634acff54fbb6fb025bab9e9ea28354c0
Ethan <ethancjones544@gmail.com>
2020-11-22 19:37:32 -0800

    Added brightness slider, colors, and saving of these

--- Python/LEDs/web_server/xmas_lights.py
+++ Python/LEDs/web_server/xmas_lights.py
@@ -21,7 +21,7 @@ led_status = "Off"
 
 # Define led pin as output
 GPIO.setup(leds, GPIO.OUT)   
-	
+
 @app.route("/")
 def index():
 	templateData = {
@@ -32,12 +32,14 @@ def index():
 	
 @app.route("/", methods = ["GET", "POST"])
 def action():
-	print("Got here!")
-	#print(request.form["effects"], flush=True)
     # Reads action and acts
 	effect = request.form["effects"]
+	brightness = float(request.form["brightness"])/100
+	color = hex_to_rgb(request.form["color"])
+
 	if effect == "on":
-		pixels.fill((255, 0, 0))
+		# Color[1], then color[0] because my lights are grb not rgb
+		pixels.fill((color[1] * brightness, color[0] * brightness, color[2] * brightness))
 	if effect == "off":
 		pixels.fill((0, 0, 0))
     #if action == "effect":
@@ -51,6 +53,10 @@ def action():
 
 	return render_template('index.html', **templateData)
 
+def hex_to_rgb(hex):
+	# Courtesy of: https://stackoverflow.com/questions/29643352/converting-hex-to-rgb-value-in-python
+	hex = hex.lstrip('#')
+	return tuple(int(hex[i:i+2], 16) for i in (0, 2, 4))
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=80, debug=True) 
