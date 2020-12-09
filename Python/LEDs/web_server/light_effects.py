867dc0e574a57e324badec6d92161fbb7961ca9a
Ethan <ethancjones544@gmail.com>
2020-12-08 23:06:50 -0800

    Implemented brightness adjuster into color creation

--- Python/LEDs/web_server/light_effects.py
+++ Python/LEDs/web_server/light_effects.py
@@ -3,28 +3,12 @@ import neopixel
 import time
 import random
 
-num_of_leds = 150
+num_of_leds = 300
 strip = neopixel.NeoPixel(board.D18, num_of_leds, auto_write = False)
 
-colors = [(0, 0, 0), (0, 0, 0), (0, 0, 0)]
-brightness = 0
-
-def alternating_colors (color0, color1, b):
-    colors[0] = color0
-    colors[1] = color1
-    global brightness
-    brightness = b
-    apply_brightness(color0, color1)
-    for i in range(0, num_of_leds - 1, 2):
-        strip[i] = colors[0]
-        strip[i + 1] = colors[1]
+def alternating_colors (color0, color1):
+    for i in range(0, num_of_leds, 2):
+        strip[i] = color0
+        strip[i + 1] = color1
     strip.show()
 
-# TODO: Maybe find a better way to apply brightness than globals?
-    # Goal was to reduce lines to minimum at the beginning of each function
-def apply_brightness(*arg):
-    for i in range(len(arg)):
-        g = arg[i][0] * brightness
-        r = arg[i][1] * brightness
-        b = arg[i][2] * brightness
-        colors[i] = (g, r, b)
--- Python/LEDs/web_server/xmas_lights.py
+++ Python/LEDs/web_server/xmas_lights.py
@@ -35,6 +35,11 @@ default_form_values = {
 	"brightness2": 50
 	}
 
+# Brightness array used to store potential multiple brightnesses
+# So colors can be updated accordingly
+# 10 zeros just so I don't have to adjust size each time I add a brightness slider
+brightness_arr = [0] * 10
+
 # Amount of colors dictionary
 effect_color_amounts = {
 	"Off": 1,
@@ -46,11 +51,15 @@ effect_color_amounts = {
 @app.route("/", methods = ["GET", "POST"])
 def action():
 	if request.method == 'POST':
+		# Copies form values to be used outside this function
+		global form_data
+		form_data = request.form.copy()
 		# Grabbing all necessary initial form elements
 		effect = get_value("effect")
 		
 		# This is a workaround since the checkbox unchecked won't POST data
 			# aka wont show up in request dict and will throw an error
+		# SO KEEP THIS AS REQUEST.FORM, NOT GET_VALUE
 		try:
 			# Converts string "True" to boolean True
 			has_mult_brightness = (request.form["mult brightnesses"] == "True")
@@ -58,8 +67,11 @@ def action():
 			has_mult_brightness = False
 
 		brightness0 = float(get_value("brightness0"))/100
+		brightness_arr[0] = brightness0
+
 		color0 = hex_to_grb(get_value("color0"))
-		brightness2 = float(get_value("brightness2"))/100
+		# Do not need to check for multiple brightnesses since first color will always be affected by first brightness
+		color0 = apply_brightness(color0)
 
 		# This is to debug what keys are actually posted currently to the form
 		#print(request.form)
@@ -69,13 +81,14 @@ def action():
 			color1 = hex_to_grb(get_value("color1"))
 			if (has_mult_brightness):
 				brightness1 = float(get_value("brightness1"))/100
-
-		# Actually applying the brightnesses
-
-
+				brightness_arr[1] = brightness1
+				print(brightness_arr)
+				color1 = apply_brightness(color1, 1)
+			else:
+				color1 = apply_brightness(color1)
 
 		if effect == "On":
-			strip.fill((color0[0] * brightness0, color0[1] * brightness0, color0[2] * brightness0))
+			strip.fill((color0[0], color0[1], color0[2]))
 			strip.show()
 
 		if effect == "Off":
@@ -87,12 +100,12 @@ def action():
 				# Running the code doesn't stop the webpage from loading
 			while True:
 				for i in range (num_of_leds):
-					strip[i] = (random.randrange(0, 255) * brightness, random.randrange(0, 255) * brightness, random.randrange(0, 255) * brightness)
+					strip[i] = (random.randrange(0, 255), random.randrange(0, 255), random.randrange(0, 255))
 				strip.show()
 				time.sleep(0.5)
 
 		if effect == "Alternating colors":
-			alternating_colors(color0, color1, brightness0)
+			alternating_colors(color0, color1)
 
 		return render_template('index.html')
 	
@@ -111,14 +124,20 @@ def hex_to_grb(hex):
 def get_value(element_name):
 	val = 0
 	try:
-		val = form.request[element_name]
-	except:
+		val = form_data[element_name]
+	except Exception as e:
 		val = default_form_values[element_name]
 
 	return val
 
-# def apply_brightness():
-# 	print("nty")
+def apply_brightness(color, brightness_index = 0):
+	brightness = brightness_arr[brightness_index]
+	print(brightness)
+	g = color[0] * brightness
+	r = color[1] * brightness
+	b = color[2] * brightness
+	color_w_brightness = (g, r, b)
+	return color_w_brightness
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=80, threaded = True) 
