257ce52dbe87a04c4add6d0015e2bf5ed2c9ef8a
Ethan <ethancjones544@gmail.com>
2021-03-09 00:20:12 -0800

    Initial commit -- Pivot to wikidex for consistent sprites

---
+++ Javascript/p5/projects/Pokeball Pokemon Comparison/wikidex_scraper.py
@@ -0,0 +1,75 @@
+import requests     # For fetching HTML
+from bs4 import BeautifulSoup   # For parsing HTML
+import xlrd     # For reading excel data (female, forms, etc)
+import urllib.request      # For saving images
+import re   # To check each name is formatted properly
+
+# SPREADSHEET DATA
+pokemon_info = xlrd.open_workbook('C:\\Users\\ejone\\OneDrive\\Desktop\\Code\\Javascript\\p5\\projects\\Pokeball Pokemon Comparison\\Pokemon Info.xls')
+sheet = pokemon_info.sheet_by_index(0)
+
+def cell_value(row, col):
+    return (sheet.cell_value(row, col))
+
+
+# WEB DATA
+game_page = requests.get("https://www.wikidex.net/index.php?title=Categor%C3%ADa:Sprites_de_Pok%C3%A9mon_Oro&fileuntil=Skarmory+oro.png#mw-category-media")
+game_page_soup = BeautifulSoup(game_page.content, 'html.parser')
+
+# TODO: Go through each gen & game? Or do self due to inconsistencies?
+# TODO: Put below in a conditional to loop if true
+print(game_page_soup.find("a", string="página siguiente"))
+# Gets pokemon name and number
+pokemon_name_number_dict = {}
+for i in range(2, 900):
+    pokemon_name_number_dict[cell_value(i, 3)] = cell_value(i, 2)
+
+# Pokemon name : Sprite link
+pokemon_sprites_dict = {}
+# Gets pokemon names for each image
+pokemon_names = []
+for name in game_page_soup.find_all(class_="gallerytext"):
+    # Doing by file extension because whitespace splits male/female, Mr.Mime, etc
+    # TODO: Somehow find file extensions used? Or allow all to pass for split
+        # Changes with game
+    pokemon_names.append(name.text.split(' espalda G1.png')[0])
+# Removing preceding new line character from names
+for i in range(len(pokemon_names)):
+    name = pokemon_names[i].split('\n')[1]
+    # Crude hardcode translation services, at your service
+    if "hembra" in name:
+        name = name.replace("hembra", "f")
+    if "macho" in name:
+        name = name.replace("macho", "m")
+    pokemon_names[i] = name
+# Adding pokedex number before name
+for i in range(len(pokemon_names)):
+    for poke, num in pokemon_name_number_dict.items():
+        if pokemon_names[i] == poke:
+            pokemon_names[i] = num + " " + pokemon_names[i]
+            break
+# Regex checker to make sure each name is formatted properly
+for name in pokemon_names:
+    if re.search("^\d\d\d\s[a-zA-Z]+", name):
+        continue
+    else:
+        print(name, "is not formatted correctly")
+
+# Gets link to pokemon sprite image
+pokemon_img_link = []
+for sprite in game_page_soup.find_all(class_="gallerybox"):
+    pokemon_img_link.append(sprite.a.img["src"])
+
+for i in range(len(pokemon_names)):
+    # Excludes japanese versions 
+    # TODO: Allow any file extension after the period
+    if re.search("\(CV\)\.png$", pokemon_names[i]):
+        print(pokemon_names[i])
+    else:
+        pokemon_sprites_dict[pokemon_names[i]] = pokemon_img_link[i]
+    # TODO: Change filename to accomodate for game
+    file_name = "Images/Pokemon/" + pokemon_names[i] + " Gen1-Back.png"
+    # Saves file
+    #urllib.request.urlretrieve(pokemon_img_link[i], file_name)
+
+#print(pokemon_sprites_dict)
