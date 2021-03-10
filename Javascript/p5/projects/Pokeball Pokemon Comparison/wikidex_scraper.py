ac30b3f8a74611bdd1cfe2e3c72463fc597526c3
Ethan <ethancjones544@gmail.com>
2021-03-10 01:21:52 -0800

    Added naming convention dict w translations

--- Javascript/p5/projects/Pokeball Pokemon Comparison/wikidex_scraper.py
+++ Javascript/p5/projects/Pokeball Pokemon Comparison/wikidex_scraper.py
@@ -19,20 +19,107 @@ sprite_page_soup = BeautifulSoup(sprite_page.content, 'html.parser')
 game_sprites_link_table = sprite_page_soup.find("table")
 games_by_gen = []
 
+# Crude translations due to the page being in spanish lol
+def game_translate(s):
+    if s == "Rojo y Azul":
+        return("Red-Blue")
+    if s == "Verde":
+        return("Red-Green")
+    if s == "Amarillo":
+        return("Yellow")
+    if s == "Oro":
+        return("Gold")
+    if s == "Plata":
+        return("Silver")
+    if s == "Cristal":
+        return("Crystal")
+    if s == "Rubí y Zafiro":
+        return("Ruby-Sapphire")
+    if s == "Esmeralda":
+        return("Emerald")
+    if s == "Rojo Fuego y Verde Hoja":
+        return("FireRed-LeafGreen")
+    if s == "Diamante y Perla":
+        return("Diamond-Pearl")
+    if s == "Platino":
+        return("Platinum")
+    if s == "Oro HeartGold y Plata SoulSilver":
+        return("HGSS")
+    if s == "Negro y Blanco":
+        return("Black-White")
+    if s == "Negro y Blanco 2":
+        return("Black2-White2")
+    if s == "X y Pokémon Y":
+        return("XY")
+    if s == "Rubí Omega y Pokémon Zafiro Alfa":
+        return("ORAS")
+    if s == "Sol y Pokémon Luna":
+        return("Sun-Moon")
+    if s == "Ultrasol y Pokémon Ultraluna":
+        return("USUM")
+    if s == "Let's Go, Pikachu! y Pokémon Let's Go, Eevee!":
+        return("Let's Go")
+    if s == "Espada y Pokémon Escudo":
+        return("Sword-Shield")
+
+def get_game(a):
+    title = a["title"].split("Pokémon ", 1)[1]
+    title = game_translate(title)
+    return(title)
+
+sprites_link_dict = {}
+# Template for file naming
+# TODO: Will probably have to add animated and static as parameters when we get to gen5+
+def sprite_link_dict_entry(gen, game, link, back = False, shiny = False):
+    keyname = ""
+    if back == False and shiny == False:
+        keyname = "Gen" + str(gen) + " " + game
+    if back == False and shiny == True:
+        keyname = "Gen" + str(gen) + " " + game + " Shiny"
+    if back == True and shiny == False:
+        keyname = "Gen" + str(gen) + "-Back"
+    if back == True and shiny == True:
+        keyname = "Gen" + str(gen) + "-Back Shiny"
+    sprites_link_dict[keyname] = link
+
 for games in game_sprites_link_table.findAll("td"):
     games_by_gen.append(games)
 
+# TODO: Exclude Let's Go
 for i in range(len(games_by_gen)):
-    # Since arrays start at 0, 5th generation == i at 4
-    # So, less than 5th gen (4th iter) are where there are no static sprites
-    if (i < 4):
-        print(games_by_gen[i].findAll("a"), "\n\n")
+    current_gen = i + 1
+    current_game = ""
+    # No animated sprites below gen5, so just get statics
+    if (current_gen < 5):
+        # Grabbing links
+        for link in games_by_gen[i].findAll("a"):
+            # Grabbing names for associated links
+            # Normal sprite 
+            if not link.text == "V" and not link.text == "E" and not link.text == "EV":
+                current_game = get_game(link)
+                sprite_link_dict_entry(current_gen, current_game, link)
+                #print(current_game, "sprite", link)
+            else:
+                if link.text == "V":
+                    #print(link.find_previous_sibling("a"))
+                    sprite_link_dict_entry(current_gen, current_game, link, shiny = True)
+                    #print(current_game, "shiny", link)
+                if link.text == "E":
+                    sprite_link_dict_entry(current_gen, current_game, link, back = True)
+                    #print("Gen" + str(current_gen) + "-Back", link)
+                if link.text == "EV":
+                    sprite_link_dict_entry(current_gen, current_game, link, back = True, shiny = True)
+                    #print("Gen" + str(current_gen) + "-Back shiny", link)
+
     # Seperates static and animated sprite pages
     else:
         for each in games_by_gen[i].findAll('b'):
             if not each.text == '|':
                 print(each.text, "\n", each.find_next_siblings("a"), "\n\n")
     #print(gen.findAll('b'))
+
+for k,v in sprites_link_dict.items():
+    print(k, ":", v)
 
 # TODO: Put below in a conditional to loop if true
 #print(game_page_soup.find("a", string="página siguiente"))
