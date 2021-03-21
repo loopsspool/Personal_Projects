f96365125f451e81a2316bf2cf58bd020039e728
Ethan <ethancjones544@gmail.com>
2021-03-21 02:49:35 -0700

    Iterates through pages and pulls data from excel database

--- Javascript/p5/projects/Pokeball Pokemon Comparison/bulba_archive_scraper.py
+++ Javascript/p5/projects/Pokeball Pokemon Comparison/bulba_archive_scraper.py
@@ -4,6 +4,27 @@ import re   # For filtering what images to download
 import urllib   # For downloading those images to my computer
 import os   # For downloading those images to my computer
 from PIL import Image   # For converting URL image data to PIL Image object 
+import xlrd     # For reading excel workbook
+
+# SPREADSHEET DATA
+pokemon_info = xlrd.open_workbook('C:\\Users\\ejone\\OneDrive\\Desktop\\Code\\Javascript\\p5\\projects\\Pokeball Pokemon Comparison\\Pokemon Info.xls')
+sheet = pokemon_info.sheet_by_index(0)
+
+def cell_value(row, col):
+    return (sheet.cell_value(row, col))
+
+def isnt_empty(row, col):
+    return (str(cell_value(row, col)) != "")
+
+def is_empty(row, col):
+    return (cell_value(row, col) == empty_cell.value)
+
+# Returns column number from column name
+def get_col_number(col_name):
+    for col in range(sheet.ncols):
+        if (cell_value(1, col) == col_name):
+            return col
+
 
 def check_if_animated(link):
     # Converting URL image to PIL Image Object
@@ -26,50 +47,112 @@ def get_largest_png(img):
     #urllib.urlretrieve(imgUrl, os.path.basename(imgUrl))
     return (src)
 
+# Gets pokemon info from excel sheet
+class Pokemon:
+    def __init__(self, name, number, gen, has_f_var, has_mega, has_giganta, reg_forms, has_type_forms, has_misc_forms, is_in_gen8):
+        self.name = name
+        self.number = number
+        self.gen = gen
+        self.has_f_var = has_f_var
+        self.has_mega = has_mega
+        self.has_giganta = has_giganta
+        self.reg_forms = reg_forms
+        self.has_type_forms = has_type_forms
+        self.has_misc_forms = has_misc_forms
+        self.is_in_gen8 = is_in_gen8
+
+# Gets column numbers from spreadsheet
+name_col = get_col_number("Name")
+num_col = get_col_number("#")
+gen_col = get_col_number("Gen")
+f_col = get_col_number("Female Variation")
+mega_col = get_col_number("Mega")
+giganta_col = get_col_number("Gigantamax")
+reg_forms_col = get_col_number("Regional Forms")
+type_forms_col = get_col_number("Type Forms")
+misc_forms_col = get_col_number("Misc Forms")
+gen8_col = get_col_number("Available in Gen 8")
+
+# Adds pokemon info from spreadsheet to object array
+print("Getting pokemon info from spreadsheet...")
+pokedex = []
+for i in range(2, 900):
+    name = cell_value(i, name_col)
+    num = cell_value(i, num_col)
+    gen = int(cell_value(i, gen_col))
+    has_f_var = isnt_empty(i, f_col)
+    has_mega = isnt_empty(i, mega_col)
+    has_giganta = isnt_empty(i, giganta_col)
+    reg_forms = cell_value(i, reg_forms_col)
+    has_type_forms = isnt_empty(i, type_forms_col)
+    has_misc_forms = isnt_empty(i, misc_forms_col)
+    is_in_gen8 = isnt_empty(i, gen8_col)
+
+    pokedex.append(Pokemon(name, num, gen, has_f_var, has_mega, has_giganta, reg_forms, has_type_forms, has_misc_forms, is_in_gen8))
+
 # Origin page (list of pokes by national pokedex)
-# starter_url = "https://archives.bulbagarden.net"
-# pokemon_starter_page = requests.get("https://archives.bulbagarden.net/wiki/Category:Pok%C3%A9mon_artwork")
-# pokemon_starter_page_soup = BeautifulSoup(pokemon_starter_page.content, 'html.parser')
+starter_url = "https://archives.bulbagarden.net"
+pokemon_starter_page = requests.get("https://archives.bulbagarden.net/wiki/Category:Pok%C3%A9mon_artwork")
+pokemon_starter_page_soup = BeautifulSoup(pokemon_starter_page.content, 'html.parser')
+save_path = "C:\\Users\\ejone\\OneDrive\\Desktop\\Code\\Javascript\\p5\\projects\\Pokeball Pokemon Comparison\\Images\\Pokemon"
 
-# pokemon_img_urls = []
-# curr_page_soup = pokemon_starter_page_soup
 
-# # Loops through pages of archives of pokemon images
-# while True:
-#     # Grabbing each individual pokemons archived image url
-#     for list_div in curr_page_soup.find_all('div', {'class': 'mw-category-group'}):
-#         for poke in list_div.find_all('li'):
-#             pokemon_img_urls.append(poke.a.get('href'))
+pokemon_img_urls = []
+curr_page_soup = pokemon_starter_page_soup
+print("Starting reading of pokemon archive links...")
 
-#     # Moving on to the next page
-#     try:
-#         next_page_url = curr_page_soup.find('a', string='next page').get('href')
-#         next_page = requests.get(starter_url + next_page_url)
-#         next_page_soup = BeautifulSoup(next_page.content, 'html.parser')
-#         curr_page_soup = next_page_soup
-#     # Unless the end of the next pages is reached
-#     except:
-#         break
+# Loops through pages of archives of pokemon images
+while True:
+    # Grabbing each individual pokemons archived image url
+    for list_div in curr_page_soup.find_all('div', {'class': 'mw-category-group'}):
+        for poke in list_div.find_all('li'):
+            # Skipping specific artwork I don't want
+            if poke.a.get('href') == "/wiki/Category:Ken_Sugimori_Pok%C3%A9mon_artwork" or poke.a.get('href') == "/wiki/Category:Official_Pok%C3%A9mon_artwork":
+                continue
+            pokemon_img_urls.append(poke.a.get('href'))
+
+    break
+
+    # Moving on to the next page
+    try:
+        next_page_url = curr_page_soup.find('a', string='next page').get('href')
+        next_page = requests.get(starter_url + next_page_url)
+        next_page_soup = BeautifulSoup(next_page.content, 'html.parser')
+        curr_page_soup = next_page_soup
+        print("Reading next page of pokemon archive links...")
+    # Unless the end of the next pages is reached
+    except:
+        print("Reached end of pokemon archive links.")
+        break
+
+
+#print(pokemon_img_urls)
+print("Processing images...")
+for i in range(len(pokemon_img_urls)):
+    # Getting relevant pokemon data
+    pokemon = pokedex[i]
+    # Getting pokemon archived image page information
+    pokemon_starter_page = requests.get(starter_url + pokemon_img_urls[i])
+    pokemon_starter_page_soup = BeautifulSoup(pokemon_starter_page.content, 'html.parser')
+    pokemon_imgs = pokemon_starter_page_soup.find_all('img')
+    # Downloading certain images
+    for img in pokemon_imgs:
+        # Drawn art
+        if not re.search("^\d\d\d[a-zA-Z].png") == None:
+            get_largest_png(img, "")
+        # Gigantamax img
+        if not re.search("Gigantamax.png$", img.attrs['alt']) == None:
+            get_largest_png(img, "")
 
 # TODO: For Diamond/Pearl, PLatinum, and HGSS check if it's animated
     # If not, open page and see if there's a file history denoted "animated" or "APNG"
-page = requests.get("https://archives.bulbagarden.net/wiki/File:Spr_4h_006_s.png")
-page_soup = BeautifulSoup(page.content, 'html.parser')
+# page = requests.get("https://archives.bulbagarden.net/wiki/File:Spr_4h_006_s.png")
+# page_soup = BeautifulSoup(page.content, 'html.parser')
 
-# Get Largest Image possible
-img_link = "https:" + get_largest_png(page_soup.find(class_ = "fullImageLink").img)
-is_animated = check_if_animated(img_link)
-
-# print(pokemon_img_urls)
-# save_path = "C:\Users\ejone\OneDrive\Desktop\Code\Javascript\p5\projects\Pokeball Pokemon Comparison\Images\Pokemon"
-# pokemon_starter_page = requests.get("https://archives.bulbagarden.net/wiki/Category:Charizard")
-# pokemon_starter_page_soup = BeautifulSoup(pokemon_starter_page.content, 'html.parser')
-# pokemon_imgs = pokemon_starter_page_soup.find_all('img')
-# for img in pokemon_imgs:
-#     # Gigantamax img
-#     if not re.search("Gigantamax.png$", img.attrs['alt']) == None:
-#         get_largest_png(img, "")
+# # Get Largest Image possible
+# # Inside page
+# img_link = "https:" + get_largest_png(page_soup.find(class_ = "fullImageLink").img)
+# is_animated = check_if_animated(img_link)
 
 
     #print(img.attrs['alt'])
-#print(charizard_imgs)
