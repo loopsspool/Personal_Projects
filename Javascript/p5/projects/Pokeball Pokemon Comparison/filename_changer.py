66c2b5a43263d8d503b24de54682dd874d142496
Ethan <ethancjones544@gmail.com>
2021-04-05 22:52:29 -0700

    Added searches for identifying tags

--- Javascript/p5/projects/Pokeball Pokemon Comparison/filename_changer.py
+++ Javascript/p5/projects/Pokeball Pokemon Comparison/filename_changer.py
@@ -1,7 +1,7 @@
 import os
 
-game_sprite_path = "C:\\Users\\ejone\\OneDrive\\Desktop\\Code\\Javascript\\p5\\projects\\Pokeball Pokemon Comparison\\Images\\Pokemon\\Game Sprites"
-files = os.listdir("C:\\Users\\ejone\\OneDrive\\Desktop")
+game_sprite_path = "C:\\Users\\ejone\\OneDrive\\Desktop\\Code\\Javascript\\p5\\projects\\Pokeball Pokemon Comparison\\Images\\Pokemon\\Game Sprites\\"
+files = os.listdir(game_sprite_path)
 file_ext = ""
 
 # TODO:
@@ -9,13 +9,41 @@ file_ext = ""
 # Then form (AND add -Form-____ tag to misc/type forms)
     # So they aren't sorted below shinies
 # Then back
-# Then by animated (AND delete static tag, only do -Animated for proper sorting)
+# Then by animated
+# Then by alt
 for f in files:
-    if f == "test.txt":
-        file_ext = f[len(f)-4 : len(f)]
-        os.rename("C:\\Users\\ejone\\OneDrive\\Desktop\\" + f, "C:\\Users\\ejone\\OneDrive\\Desktop\\new_test" + file_ext)
-        print(f)
-    #if " static" in f:
+    shiny = False
+    female = False
+    mega = False
+    gigantamax = False
+    region = ""
+    form = ""
+    back = False
+    animated = False
+    alt = False
     
-    #print(f, "\n")
+    if "Shiny" in f:
+        shiny = True
+    if f.endswith("f.png") or f.endswith("f alt.png") or f.endswith("f.gif")or f.endswith("f alt.gif"):
+        female = True
+    if "Mega" in f and not "Meganium" in f:
+        mega = True
+    if "Gigantamax" in f:
+        gigantamax = True
+    if "Alolan" in f:
+        region = "Alola"
+    if "Galarian" in f:
+        region = "Galar"
+    # TODO: For forms split by spaces then do last in the list and lop of file extension?
+        # Except if it contains alt, then do second to last?
+    if "Back" in f:
+        back = True
+    if "Animated" in f:
+        animated = True
+    if " alt" in f:
+        alt = True
+
+    file_ext = f[len(f)-4 : len(f)]
+    #new_name = 
+    #os.rename(game_sprite_path + f, game_sprite_path + new)
 
