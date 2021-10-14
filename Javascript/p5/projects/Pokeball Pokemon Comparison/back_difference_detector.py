91f655f031cba32bbe5b16ac00c4321c07c5e643
Ethan <ethancjones544@gmail.com>
2021-10-13 23:13:37 -0700

    Initial commit

---
+++ Javascript/p5/projects/Pokeball Pokemon Comparison/back_difference_detector.py
@@ -0,0 +1,7 @@
+# TODO: Back sprites for LGPE are showing in excel checklist because game isn't in filename for back sprites
+    # Since filename only contains Gen7, the SM-USUM photos trigger the "x" in the LGPE column
+        # They ARE different because SMUSUM was on the 3ds, LGPE is on the Switch
+
+# TODO: This script will take all back sprites of each game in a generation up to gen 4 to compare them and see if they're different
+    # If they are different between games in a gen, add game to the filename
+    # Otherwise, just gen is fine
